5b2c83efa786918e0aac2876a44cc5f7e206be34
ShihabMahmod <58717265+ShihabMahmod@users.noreply.github.com>
2024-08-03 14:55:29 +0600

    fetch user

--- controllers/auth/LoginController.js
+++ controllers/auth/LoginController.js
@@ -11,7 +11,7 @@ class LoginController {
 
       try {
         const { email, password } = req.body;
-        return res.json(req.body);
+        
         const isValidate = await Validator.loginValidation(req.body);
         if (isValidate.success != true) {
             return res.status(400).json({ errors: isValidate });
@@ -21,6 +21,7 @@ class LoginController {
             email: email,
             },
         });
+        return res.json(user);
         if (user) {
             if(user.is_approved == 1) {
                 const token = generateToken(user);
