eec79f1692387025ab9479928e906e1eed79683e
ShihabMahmod <58717265+ShihabMahmod@users.noreply.github.com>
2024-08-17 15:42:26 +0600

    update6778

--- server.js
+++ server.js
@@ -30,7 +30,13 @@ import Routes from "./routes/index.js";
 const app = express();
 const PORT = process.env.PORT || 5000;
 
-app.use(cors());
+app.use(
+  cors({
+    origin: '*', // Allow requests from any origin
+    methods: 'GET,HEAD,PUT,PATCH,POST,DELETE',
+    credentials: true, // Allow cookies to be sent
+  })
+);
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 
