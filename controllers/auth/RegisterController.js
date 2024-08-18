2af446af56431bbc62c01c8168196d40d3f0c100
ShihabMahmod <58717265+ShihabMahmod@users.noreply.github.com>
2024-08-18 11:34:25 +0600

    Update security

--- controllers/auth/RegisterController.js
+++ controllers/auth/RegisterController.js
@@ -179,13 +179,12 @@ static async store(req, res) {
   
     res.cookie('cookie_data', JSON.stringify(cookieData), {
         httpOnly: true, 
-        secure: process.env.NODE_ENV === 'production', 
+        
         maxAge: 24 * 60 * 60 * 1000,
-        sameSite: 'None',
     });
 
     setTimeout(() => {
-      res.redirect("http://localhost:3000/");
+      res.redirect("http://localhost:3000");
     }, 5000);
    
     
