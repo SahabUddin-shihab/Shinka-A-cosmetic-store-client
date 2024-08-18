1b3ab52aaeba7fd6ccd7cf42d25c7163eb152484
ShihabMahmod <58717265+ShihabMahmod@users.noreply.github.com>
2024-08-18 11:25:56 +0600

    update coocie2

--- controllers/auth/RegisterController.js
+++ controllers/auth/RegisterController.js
@@ -181,7 +181,7 @@ static async store(req, res) {
         httpOnly: true, 
         secure: process.env.NODE_ENV === 'production', 
         maxAge: 24 * 60 * 60 * 1000,
-        //sameSite: 'None',
+        sameSite: 'None',
     });
 
     setTimeout(() => {
